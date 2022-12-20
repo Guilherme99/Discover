77a6c5b559cbdbb2d7a734aa6226e8ac754f9bef
Guilherme <italoguilherme@lapisco.ifce.edu.br>
2022-12-20 11:52:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import styles from "./app.module.css";
+import { Sidebar } from "./components/Sidebar";
+import "./global.css";
+
+const Posts = [
+  {
+    author: "Italo",
+    content: "Loren Sputas",
+  },
+  {
+    author: "Joana",
+    content: "Loren Sputas",
+  },
+  {
+    author: "Belissa",
+    content: "Loren Sputas",
+  },
+];
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {Posts?.map((post) => (
+            <Post author={post?.author} content={post?.content} />
+          ))}
+        </main>
+      </div>
+    </>
+  );
+}
